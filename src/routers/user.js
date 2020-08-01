e1afc9ca351b52da6d153f183521ef2fdb34d04e
Itzik Shachar <itziks@netomed.local>
2020-08-01 21:35:07 +0300

    adding jwt

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,14 +2,27 @@ const express = require('express')
 const router = new express.Router()
 module.exports = router
 const User = require('../models/user')
+const { urlencoded } = require('express')
 
 router.post('/users', async (req, res) => {
     try {
         const user = new User(req.body)
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
     }
 })
 
@@ -52,7 +65,10 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
         if (!user) {
             return res.status(404).send();
         }
